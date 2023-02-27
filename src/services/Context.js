cecc5ac9a0ae8b5550ac3ec33f5a45433f57f363
gauraviquanta <gauravluthra.iquanta@gmail.com>
2023-02-27 12:30:16 +0530

    cx

--- src/services/Context.js
+++ src/services/Context.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+import { useParams } from 'react-router-dom';
 
 
 export const Context = React.createContext();
@@ -8,7 +9,8 @@ export function useAuth() {
 }
 
 export const ContextProvider = ({ children }) => {
-  const[attemptID,setattemptID]=useState('')
+  const params =useParams();
+  const [attemptID, setattemptID] = useState("");
   const [responseReceived, setResponseReceived] = useState(false);
 
   // useEffect(() => {
@@ -24,37 +26,75 @@ export const ContextProvider = ({ children }) => {
   //   fetchData();
   // }, []);
 
+  const createAttemptId = () => {
+    const jsonData = {
+      name: "Gaurav",
+      email: "asdnf@gmail.com",
+      uid: "2345678098765",
+      mockId: "ruksdjhfjdksfgkdfg",
+    };
+
+    fetch(`http://43.204.36.216:8000/api/student/v1/mocks`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jsonData),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setattemptID(data.answersheet._id);
+        setResponseReceived(true);
+      })
+      .catch((error) => console.error(error));
+  };
 
  
 
-  const createAttemptId=()=>{
-    const jsonData=
-    {
-    name: "Gaurav",
-    email: "asdnf@gmail.com",
-    uid: "2345678098765",
-    mockId: "ruksdjhfjdksfgkdfg"
-}
+  //Timer code
 
-    fetch(`http://43.204.36.216:8000/api/student/v1/mocks`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(jsonData)
-    })
-    .then(response => response.json())
-    .then(data => {console.log(data)
-    setattemptID(data.answersheet._id )
-    setResponseReceived(true)}
-    
-    )
-    .catch(error => console.error(error));
+  const [seconds, setSeconds] = useState(2400); // 40 minutes in seconds
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds - 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0  ) {
+      clearInterval(interval);
     }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+
+
+  const startTimer = () => {
+    setIsActive(true);
+  };
+
+  const stopTimer = () => {
+    setIsActive(false);
+  };
+
+  const resetTimer = () => {
+    setSeconds(2400);
+    setIsActive(false);
+  };
+
+  
+ 
 
   return (
     <>
-      <Context.Provider value={{createAttemptId,responseReceived,attemptID}}>{children}</Context.Provider>
+      <Context.Provider
+        value={{ createAttemptId, responseReceived, 
+        attemptID,seconds, startTimer, stopTimer, resetTimer,  }}
+      >
+        {children}
+      </Context.Provider>
     </>
   );
 };
