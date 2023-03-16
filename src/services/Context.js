f3aaf086ca107279f836d894298ee03cae466b16
gauraviquanta <gauravluthra.iquanta@gmail.com>
2023-03-16 16:09:30 +0530

    Attemp id api cahnges done, analysis Ui improve

--- src/services/Context.js
+++ src/services/Context.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import request from "./Request";
 
 export const Context = React.createContext();
@@ -11,7 +11,7 @@ export const ContextProvider = ({ children }) => {
   const [responseReceived, setResponseReceived] = useState(false);
   const [buttonLoading, setButtonLoading] = useState(false);
   const [analysisData, setAnalysisData] = useState({});
-  const [isLoading, setLoading] = useState(false);
+  const [isLoading, setLoading] = useState(true);
 
   const createAttemptId = () => {
     console.log("create attempt call");
@@ -20,7 +20,7 @@ export const ContextProvider = ({ children }) => {
       name: "Gaurav",
       email: "asdnf@gmail.com",
       uid: "2345678098765",
-      mockId: "ruksdjhfjdksfgkdfg",
+      mockId: `${process.env.REACT_APP_MOCK_ID}`,
     };
 
     fetch(`${process.env.REACT_APP_BASE_URL}/api/student/v1/mocks`, {
@@ -33,9 +33,9 @@ export const ContextProvider = ({ children }) => {
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setattemptID(data.answersheet._id);
-        setResponseReceived(true);
+        setattemptID(data.attemptId);
         setButtonLoading(false);
+        return setResponseReceived(true);
       })
       .catch((error) => {
         console.error("Attempt id Response not receive", error);
@@ -67,6 +67,7 @@ export const ContextProvider = ({ children }) => {
           buttonLoading,
           analysisData,
           analysisDataApi,
+          isLoading,
         }}
       >
         {children}
