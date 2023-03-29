b67e4cd1e6fb426dab87f999109eb06cbb9d8dbd
gauraviquanta <gauravluthra.iquanta@gmail.com>
2023-03-29 20:49:54 +0530

    ui responsive done

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,27 +1,18 @@
 import CenterMain from "./CenterMain";
 import Header from "./Header";
 import React from "react";
-
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
+import Slide from "@mui/material/Slide";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-function Main() { 
-
+function Main() {
   return (
-    <div>     
-    <Header />
-      <CenterMain  />
-      </div>
-   
+    <div className="overflow-hidden">
+      <Header />
+      <CenterMain />
+    </div>
   );
 }
 
