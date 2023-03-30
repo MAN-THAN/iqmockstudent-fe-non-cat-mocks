4ec8f1dfa52d4c8f470d1370d93f92e6ba95479b
gauraviquanta <gauravluthra.iquanta@gmail.com>
2023-03-30 10:20:18 +0530

    Ui responsive latest header adjustment done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import { Typography } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import "../styleSheets/header.css"
-import { useAuth } from '../services/Context'
+
 
 function Header() {
     const userName = JSON.parse(localStorage.getItem("userData"))?.name;
@@ -12,19 +12,19 @@ function Header() {
    
     return (
         <header className="p-0 m-0 text-bg-dark">
-            <div className="container-fluid py-1 ">
+            <div className="container-fluid  ">
                 <div className="d-flex flex-wrap align-items-center justify-content-between justify-content-lg-between py-2 px-3">
                     <div className='flex-item ' >
                         <Link to="/">
-                        <img src={require("../images/iquanta_Logo.png")} alt="iquanta_logo" className='img-fluid iquanta_logo'/></Link>
+                        <img src={require("../images/iquanta_Logo.png")} width={100} alt="iquanta_logo" className='img-fluid  '/></Link>
                     </div>
-                    <div className='flex-item  ms-5 ps-5' >
-                        <Typography variant='h4'>iCAT 1.O 2023</Typography>
+                    <div className='flex-item pt-1 ms-5 ps-5' >
+                        <Typography variant='h4' sx={{fontSize:"25px"}}>iCAT 1.O 2023</Typography>
                     </div>
 
-                    <div className="d-flex gap-3   justify-self-end">
+                    <div className="d-flex gap-3  justify-self-end">
                         <div className='text-end'>
-                            <Typography sx={{ fontFamily: "var(--font-inder)", fontSize: "21px", fontWeight: 400 }} >{ userName }</Typography>
+                            <Typography sx={{ fontFamily: "var(--font-inder)", fontSize: "20px", fontWeight: 400 }} >{ userName }</Typography>
                             <Typography sx={{ fontFamily: "var(--font-inder)", fontSize: "13px", fontWeight: 400 }}>User id : { userID}</Typography>
                         </div>
                         <div className=''>
