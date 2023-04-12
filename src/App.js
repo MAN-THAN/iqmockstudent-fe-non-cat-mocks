902883e736827fd4daae18e9667d580370142245
MANTHAN TYAGI <manthantyagi04@gmail.com>
2023-04-12 11:20:25 +0530

    view solution bottom ui

--- src/App.js
+++ src/App.js
@@ -14,28 +14,28 @@ import OverallAnalysis from "./Analysis/OverallAnalysis";
 import ViewSolution from "./Components/ViewSolution";
 import UserAuth from "./Components/UserAuth";
 import LeaderBoard from "./LeaderBoard.js/LeaderBoard";
+import TempCompo from "./Components/tempCompo";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-   
-        <Routes>
-          <Route path="/" element={<Instructions />} />
-          <Route path="/terms/:name/:email/:uid/:mockId" element={<Terms />} />
-          <Route path="/user_authentication/:name/:email/:uid/:mockId" element={<UserAuth />} />
-          <Route path="/main/:mockId/:type" element={<Protected Comp={Main} />} />
+      <Routes>
+        <Route path="/" element={<Instructions />} />
+        <Route path="/terms/:name/:email/:uid/:mockId" element={<Terms />} />
+        <Route path="/user_authentication/:name/:email/:uid/:mockId" element={<UserAuth />} />
+        <Route path="/main/:mockId/:type" element={<Protected Comp={Main} />} />
 
-          <Route path="/analysis/:mockId/:attemptId" element={<AnalysisMain />}>
-            <Route path="topicwise" element={<TopicAnalysis />} />
-            <Route path="sectionwise/:subject" element={<SectionAnalysis />} />
-            <Route path="overall" element={<OverallAnalysis />} />
-            <Route path="difficulty" element={<DifficultyAnalysis />} />
-          </Route>
-          <Route path="/leaderboard/:mockId/:attemptId" element={<LeaderBoard/>} />
-          <Route path="/viewsolutions/:attemptId/:mocktype" element={<ViewSolution />} />
-          <Route path="/main/:mockid/:type" element={<Protected Comp={Main} />} />
-        </Routes>
-    
+        <Route path="/analysis/:mockId/:attemptId" element={<AnalysisMain />}>
+          <Route path="topicwise" element={<TopicAnalysis />} />
+          <Route path="sectionwise/:subject" element={<SectionAnalysis />} />
+          <Route path="overall" element={<OverallAnalysis />} />
+          <Route path="difficulty" element={<DifficultyAnalysis />} />
+        </Route>
+        <Route path="/leaderboard/:mockId/:attemptId" element={<LeaderBoard />} />
+        <Route path="/viewsolutions/:attemptId/:mocktype" element={<ViewSolution />} />
+        <Route path="/main/:mockid/:type" element={<Protected Comp={Main} />} />
+        <Route path="/tempCompo" element={<TempCompo />} />
+      </Routes>
     </ThemeProvider>
   );
 }
