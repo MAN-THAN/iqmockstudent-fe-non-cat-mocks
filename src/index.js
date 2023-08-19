c963f736374535636ce017d384496698c66e1b01
MAN-THAN <manthantyagi04@gmail.com>
2023-08-19 13:02:02 +0530

    redux persist implemented

--- src/index.js
+++ src/index.js
@@ -7,15 +7,19 @@ import { ContextProvider } from "./services/Context";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store ={store}>
-  <BrowserRouter>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <ContextProvider>
+          <App />
+        </ContextProvider>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
 //some addons
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userDataSlice from "./slices/userDataSlice";
 import mockDataSlice from "./slices/mockDataSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+import { combineReducers } from "@reduxjs/toolkit";
 
-const store = configureStore({
-    reducer : {
-        userData : userDataSlice,
-        mockData : mockDataSlice
-    }
-})
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-export {store};
+const rootReducer = combineReducers({
+  userData: userDataSlice,
+  mockData: mockDataSlice,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware : [thunk]
+});
+
+export const persistor = persistStore(store);
